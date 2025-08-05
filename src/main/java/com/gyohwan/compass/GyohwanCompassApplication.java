c78726ea796ddebdabd470da3dd928b8f0258b90
Wibaek Park <devmuromi@gmail.com>
2025-08-06 03:50:15 +0900

    init

---
+++ src/main/java/com/gyohwan/compass/GyohwanCompassApplication.java
@@ -0,0 +1,13 @@
+package com.gyohwan.compass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyohwanCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyohwanCompassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyohwan/compass/GyohwanCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyohwan.compass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyohwanCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
