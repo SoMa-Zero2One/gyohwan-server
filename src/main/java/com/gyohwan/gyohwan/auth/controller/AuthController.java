3bf8b5a5e1fbed3cc14695fc594b078591f0efbe
Wibaek Park <devmuromi@gmail.com>
2025-09-29 17:27:29 +0900

    chore: auth api path 수정

--- src/main/java/com/gyohwan/gyohwan/auth/controller/AuthController.java
+++ src/main/java/com/gyohwan/gyohwan/auth/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/v1/auth")
 @RestController
 public class AuthController {
 
